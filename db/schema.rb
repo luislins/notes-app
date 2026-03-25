086e40291ad123c3c4a627cc1adb6e41d2c877f4
Luis Guilherme <luisguilherme.siqueira32@gmail.com>
2026-03-25 14:49:52 -0300

    Associate notes with authenticated user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_25_140912) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_25_171238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,29 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_25_140912) do
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.string "token"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.bigint "user_id", null: false
+    t.index ["token"], name: "index_sessions_on_token", unique: true
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "notes", "users"
+  add_foreign_key "sessions", "users"
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,24 +1,28 @@
 require "test_helper"
 
 class NoteTest < ActiveSupport::TestCase
+  setup do
+    @user = users(:one)
+  end
+
   test "valid with title and content" do
-    note = Note.new(title: "Título", content: "Conteúdo")
+    note = @user.notes.build(title: "Título", content: "Conteúdo")
     assert note.valid?
   end
 
   test "valid with title only" do
-    note = Note.new(title: "Título")
+    note = @user.notes.build(title: "Título")
     assert note.valid?
   end
 
   test "invalid without title" do
-    note = Note.new(title: nil)
+    note = @user.notes.build(title: nil)
     assert_not note.valid?
     assert_includes note.errors[:title], "can't be blank"
   end
 
   test "invalid with empty title" do
-    note = Note.new(title: "")
+    note = @user.notes.build(title: "")
     assert_not note.valid?
   end
 end
